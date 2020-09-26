d8399a72612eac4d0eb79db52ad107e8410875c1
richenyunqi <csjiangfeng@gmail.com>
2020-09-26 13:29:39 +0800

    update针对无权图单源最短路的BFS算法

--- 图/最短路径问题/针对无权图单源最短路的BFS算法.cpp
+++ 图/最短路径问题/针对无权图单源最短路的BFS算法.cpp
@@ -1,19 +1,22 @@
-//无权图单源最短路的BFS算法的边除首尾结点外，边不包含其他信息，不需要使用上面的存储结构，graph[i]中存储的直接是i号结点能到达的结点编号
-vector<gg> graph[MAX];  // graph[i][j]表示结点i能到达的第j个结点编号
-void BFS(gg s) {
+vector<vector<gg>> graph(MAX);
+// dis存储到达所有结点的最短路径长度
+vector<gg> dis(MAX);
+void bfs(gg v) {
+    vector<bool> inQueue(MAX);
     queue<gg> q;
-    memset(inQueue, 0, sizeof(inQueue));
-    dis[s] = 0;
-    q.push(s);
-    inQueue[s] = true;
-    while (!q.empty()) {
-        gg p = q.front();
-        q.pop();
-        for (gg i : graph[p]) {
-            if (!inQueue[i]) {
-                q.push(i);
-                dis[i] = dis[p] + 1;
-                inQueue[i] = true;
+    q.push(v);
+    inQueue[v] = true;
+    for (gg d = 0; not q.empty(); ++d) {
+        gg s = q.size();
+        for (gg i = 0; i < s; ++i) {
+            v = q.front();
+            q.pop();
+            dis[v] = d;
+            for (gg i : graph[v]) {
+                if (not inQueue[i]) {
+                    q.push(i);
+                    inQueue[i] = true;
+                }
             }
         }
     }
