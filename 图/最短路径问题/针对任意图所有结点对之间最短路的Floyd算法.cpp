9da7089331c8202008d110a2cef3dbe41d3872d0
richenyunqi <csjiangfeng@gmail.com>
2020-09-26 14:25:15 +0800

    更新Floyd算法

--- 图/最短路径问题/针对任意图所有结点对之间最短路的Floyd算法.cpp
+++ 图/最短路径问题/针对任意图所有结点对之间最短路的Floyd算法.cpp
@@ -1,5 +1,8 @@
 //图用邻接矩阵存储，若为有权图，graph[i][i]=0,有权的即为权值，其余均为INT_MAX
 //若为无权图，若i和j之间有边，graph[i][j]=1；其余均为0
+vector<vector<gg>> graph(MAX, vector<gg>(MAX));
+// dis存储到达所有结点对之间的最短路径长度
+vector<gg> dis(MAX, vector<gg>(MAX));
 void floyd() {
     for (gg i = 0; i < ni; ++i) {
         for (gg j = 0; j < ni; ++j) {
--- 图/最短路径问题/针对正权图单源最短路的Dijkstra算法.cpp
+++ 图/最短路径问题/针对正权图单源最短路的Dijkstra算法.cpp
@@ -1,6 +1,7 @@
 typedef pair<gg, gg> pii;
 void Dijkstra(gg s) {
-    priority_queue<pii, vector<pii>, greater<pii>> pq;  // pii的first成员存储dis，second成员存储结点编号
+    priority_queue<pii, vector<pii>, greater<pii>>
+        pq;  // pii的first成员存储dis，second成员存储结点编号
     fill(dis, dis + MAX, LONG_LONG_MAX);
     dis[s] = 0;
     pq.push({0, s});
