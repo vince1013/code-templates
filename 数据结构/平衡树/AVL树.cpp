e3d18721859fd0a2af8c70be8a0f992cc86cd6ea
richenyunqi <csjiangfeng@gmail.com>
2020-10-16 09:33:59 +0800

    更新AVL树

--- 数据结构/平衡树/AVL树.cpp
+++ 数据结构/平衡树/AVL树.cpp
@@ -1,17 +1,21 @@
+//与根结点数据域相等时，一律插入到右子树
 struct AVLTreeNode {
-    int data, height, num;  //关键字、高度
+    gg data, height, num;  //关键字、高度
     AVLTreeNode *left, *right;
-    AVLTreeNode(int value, AVLTreeNode* l = nullptr, AVLTreeNode* r = nullptr) :
-        data(value), height(0), left(l), right(r) {}
+    AVLTreeNode(gg v, AVLTreeNode* l = nullptr, AVLTreeNode* r = nullptr,
+                gg h = 0) :
+        data(v),
+        height(h), left(l), right(r) {}
 };
-int getHeight(AVLTreeNode* r) { return r == nullptr ? 0 : r->height; }
-AVLTreeNode* findAVL(AVLTreeNode* root, int data) {
-    if (root == nullptr || root->data == data)
+gg getHeight(AVLTreeNode* r) { return r ? r->height : 0; }
+AVLTreeNode* findAVL(AVLTreeNode* root, gg data) {
+    if (not root or root->data == data) {
         return root;
-    if (data < root->data)
+    } else if (data < root->data) {
         return findAVL(root->left, data);
-    else
+    } else {
         return findAVL(root->right, data);
+    }
 }
 // LL：左左对应的情况(左单旋转),返回旋转后的根节点
 AVLTreeNode* leftLeftRotation(AVLTreeNode* k2) {
@@ -42,75 +46,76 @@ AVLTreeNode* rightLeftRotation(AVLTreeNode* k1) {
     return rightRightRotation(k1);
 }
 //将结点插入到AVL树中，并返回根节点
-AVLTreeNode* insertNode(AVLTreeNode*& root, int data) {
-    if (root == nullptr)
+AVLTreeNode* insertNode(AVLTreeNode*& root, gg data) {
+    if (not root) {
         root = new AVLTreeNode(data);
-    else if (data < root->data) {
+    } else if (data < root->data) {
         root->left = insertNode(root->left, data);
         // 插入节点后，若AVL树失去平衡，则进行相应的调节。
         if (getHeight(root->left) - getHeight(root->right) == 2) {
-            if (data < root->left->data)
+            if (data < root->left->data) {
                 root = leftLeftRotation(root);
-            else
+            } else {
                 root = leftRightRotation(root);
+            }
         }
-    } else if (data > root->data) {  // 应该将data插入到"root的右子树"的情况
+    } else {  // 应该将data插入到"root的右子树"的情况
         root->right = insertNode(root->right, data);
         // 插入节点后，若AVL树失去平衡，则进行相应的调节。
         if (getHeight(root->right) - getHeight(root->left) == 2) {
-            if (data > root->right->data)
+            if (data > root->right->data) {
                 root = rightRightRotation(root);
-            else
+            } else {
                 root = rightLeftRotation(root);
+            }
         }
-    } else
-        cout << "添加失败：不允许添加相同的节点！" << endl;
+    }
     root->height = max(getHeight(root->left), getHeight(root->right)) + 1;
     return root;
 }
 //查找最大结点：返回root为根结点的AVL树的最大结点。
 AVLTreeNode* maximum(AVLTreeNode* root) {
-    if (root == nullptr)
-        return nullptr;
-    while (root->right != nullptr)
+    while (root and root->right) {
         root = root->right;
+    }
     return root;
 }
 //查找最小结点：返回tree为根结点的AVL树的最小结点。
 AVLTreeNode* minimum(AVLTreeNode* root) {
-    if (root == nullptr)
-        return nullptr;
-    while (root->left != nullptr)
+    while (root and root->left)
         root = root->left;
     return root;
 }
 //删除结点(z)，返回根节点
 AVLTreeNode* remove(AVLTreeNode*& root, AVLTreeNode* z) {
     // 根为空或者没有要删除的节点，直接返回nullptr。
-    if (root == nullptr || z == nullptr)
+    if (not root or not z) {
         return nullptr;
+    }
     if (z->data < root->data) {  // 待删除的节点在"root的左子树"中
         root->left = remove(root->left, z);
         // 删除节点后，若AVL树失去平衡，则进行相应的调节。
         if (getHeight(root->right) - getHeight(root->left) == 2) {
             AVLTreeNode* r = root->right;
-            if (getHeight(r->left) > getHeight(r->right))
+            if (getHeight(r->left) > getHeight(r->right)) {
                 root = rightLeftRotation(root);
-            else
+            } else {
                 root = rightRightRotation(root);
+            }
         }
     } else if (z->data > root->data) {  // 待删除的节点在"root的右子树"中
         root->right = remove(root->right, z);
         // 删除节点后，若AVL树失去平衡，则进行相应的调节。
         if (getHeight(root->left) - getHeight(root->right) == 2) {
             AVLTreeNode* l = root->left;
-            if (getHeight(l->right) > getHeight(l->right))
+            if (getHeight(l->right) > getHeight(l->right)) {
                 root = leftRightRotation(root);
-            else
+            } else {
                 root = leftLeftRotation(root);
+            }
         }
     } else {  // root是对应要删除的节点。
-        if ((root->left != nullptr) && (root->right != nullptr)) {
+        if (root->left and root->right) {
             if (getHeight(root->left) > getHeight(root->right)) {
                 // 如果root的左子树比右子树高；
                 // 则(01)找出root的左子树中的最大节点
@@ -134,14 +139,15 @@ AVLTreeNode* remove(AVLTreeNode*& root, AVLTreeNode* z) {
             }
         } else {
             AVLTreeNode* tmp = root;
-            root = (root->left != nullptr) ? root->left : root->right;
+            root = root->left ? root->left : root->right;
             delete tmp;
         }
     }
     return root;
 }
-void remove(AVLTreeNode*& root, int data) {
+void remove(AVLTreeNode*& root, gg data) {
     AVLTreeNode* z;
-    if ((z = findAVL(root, data)) != nullptr)
+    if ((z = findAVL(root, data))) {
         root = remove(root, z);
+    }
 }
